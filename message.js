50d50a3b374edf7a7b2ce19f1e7376c6c9ab3d79
DSeven <diego.imt7@gmail.com>
2021-01-17 15:37:18 -0600

    Messages corrections.

--- message.js
+++ message.js
@@ -1,21 +1,23 @@
-const superHeroNames = ['Captain America: ', 'Thor: ', 'Black Panter: ', 'Iron Man: ', 'Hulk: ']; //Different super heores that can tell the message
-const commonQuotesStarters = ['Remember, ', 'Never forget, ', 'Always keep in mind, ', 'As I always say, ']; //Different ways to start the message
-const commonTopics = ['the truth ', 'justice ', 'love ', 'life ']; // Different topics for the corresponding message
-const mainMessage = ['will always be there, wether we can see it or not.', 'is a natural part of us.', 'is the most important thing we have.']; 
+const messageMaterial = {
+    superHeroNames: ['Captain America: ', 'Thor: ', 'Black Panter: ', 'Iron Man: ', 'Hulk: '], //Different super heores that can tell the message
+    commonQuotesStarters: ['Remember, ', 'Never forget, ', 'Always keep in mind, ', 'As I always say, '], //Different ways to start the message
+    commonTopics: ['the truth ', 'justice ', 'love ', 'life '], // Different topics for the corresponding message
+    mainMessage: ['will always be there, whether we can see it or not.', 'is a natural part of us.', 'is the most important thing we have.']
 
+}
 
-const messageGenerator = (part1, part2, part3, part4) => { // fucntion that generates the messages
+const messageGenerator = (materials) => { // fucntion that generates the messages
     let message; 
-    message = part1[Math.floor(Math.random()*part1.length)];
+    message = materials.superHeroNames[Math.floor(Math.random()*materials.superHeroNames.length)];
     if(message == 'Hulk: '){
-        message+= 'Hulk smaaaash!';
+        message += 'Hulk smaaaash!';
     }
     else {
-        message += part2[Math.floor(Math.random()*part2.length)];
-        message += part3[Math.floor(Math.random()*part3.length)];
-        message += part4[Math.floor(Math.random()*part4.length)];
+        message += materials.commonQuotesStarters[Math.floor(Math.random()*materials.commonQuotesStarters.length)];
+        message += materials.commonTopics[Math.floor(Math.random()*materials.commonTopics.length)];
+        message += materials.mainMessage[Math.floor(Math.random()*materials.mainMessage.length)];
     }
     return message;
 }
 
-console.log(messageGenerator(superHeroNames, commonQuotesStarters, commonTopics, mainMessage));
+console.log(messageGenerator(messageMaterial));
